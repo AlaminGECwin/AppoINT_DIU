1207bd12f49c8c94ea1b20dc603e32d5be3538d6
AlaminGECwin <67990187+AlaminGECwin@users.noreply.github.com>
2020-09-15 10:52:51 +0600

    I have completed AppoINT
    This is Appoint management software. by what we can council or get counselling from others.

---
+++ model/configuration.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 
+ */
+class Database{
+	
+	function __construct()
+	{
+		# code...
+		//echo "<br>i am database";
+	}
+	private $host;
+	private $dbusername;
+	private $dbpassword;
+	private $dbname;
+	
+	protected function connect(){
+		$this->host='localhost';
+		$this->dbusername='root';
+		$this->dbpassword='';
+		$this->dbname='counselling_manager';
+		try{
+			$connection=new PDO("mysql:host=$this->host;dbname=$this->dbname",$this->dbusername,$this->dbpassword);
+		}catch(PDOExection $e){
+			echo $e->getMessage();
+		}
+		return $connection;
+	}
+
+	
+}
